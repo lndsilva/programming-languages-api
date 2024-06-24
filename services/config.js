ee0062c869ba397b88793c7d290bc247713741d7
lndsilva <lndsilva@hotmail.com>
2024-06-24 11:56:29 -0300

    criando crud mysql

---
+++ services/config.js
@@ -0,0 +1,11 @@
+const config = {
+    db: {
+        host: "mysql2",
+        user: "senac",
+        password: "senac",
+        database: "dbRest",
+        connectTimeout: 6000
+    },
+    listPerPage: 10,
+}
+module.exports = config
